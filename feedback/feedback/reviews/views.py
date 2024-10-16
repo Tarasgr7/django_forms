cd014fb7df0f2f8eaa89cf41b82b95b06f172416
Tarasgr7 <114420340+Tarasgr7@users.noreply.github.com>
2024-10-17 00:09:24 +0300

    Add files via upload

---
+++ feedback/feedback/reviews/views.py
@@ -0,0 +1,25 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .forms import ReviewForm
+from .models import Review
+from django.views import View
+
+class ReviewView(View):
+    def get(self,request):
+        form=ReviewForm()
+        
+        return render(request,"reviews/review.html",{
+            "form":form
+            })
+    def post(self,request):
+        form=ReviewForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect("/thank_you")
+        return render(request,"reviews/review.html",{
+        "form":form
+        })
+
+
+def thank_you(request):
+    return render(request,"reviews/thank_you.html")
